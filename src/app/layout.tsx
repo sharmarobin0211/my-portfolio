2e4cb8a1cd643413d1c7013d9c175e0418af33b6
SuperSection <soumosarkar297@gmail.com>
2025-03-18 01:49:34 +0530

    Update metadata with new title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Soumo Sarkar",
+  description: "DevOps Engineer & Full-stack Developer",
 };
 
 export default function RootLayout({
