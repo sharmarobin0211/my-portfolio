c9f8c0d383293be968f239348ddb89211885c4bf
SuperSection <soumosarkar297@gmail.com>
2025-03-20 08:23:10 +0530

    fix(Card): adjust padding in Card component for compatible styling

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -54,7 +54,7 @@ export const ExpertiseSection = () => {
         <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
           <div className="flex gap-8 flex-none">
           {skills.map(skill => (
-            <Card key={skill.name} className="max-w-xs md:max-w-md md:p-8">
+            <Card key={skill.name} className="max-w-xs md:max-w-md p-6 md:p-8">
               <div className="flex gap-4 items-center">
                 <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
                   <Image
