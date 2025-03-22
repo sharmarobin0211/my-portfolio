cac7ce680c26f588e5fe582dad1f716d0355ce03
SuperSection <soumosarkar297@gmail.com>
2025-03-22 10:09:27 +0530

    feat(HeroOrbit): enhance HeroOrbit component with orbit and spin animations

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,27 +1,53 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+
+interface HeroOrbitProps {
+  size: number;
+  rotation: number;
+  shouldOrbit?: boolean;
+  orbitDuration?: string;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+}
+
 
 export const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  shouldOrbit = false,
+  orbitDuration,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<HeroOrbitProps>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
       <div
-        className="flex items-start justify-start"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          height: `${size}px`,
-          width: `${size}px`,
-        }}
+        className={twMerge(shouldOrbit === true && "animate-spin")}
+        style={{ animationDuration: orbitDuration }}
       >
         <div
-          className="inline-flex"
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            transform: `rotate(${rotation}deg)`,
+            height: `${size}px`,
+            width: `${size}px`,
           }}
         >
-          {children}
+          <div
+            className={twMerge(shouldSpin === true && "animate-spin")}
+            style={{ animationDuration: spinDuration }}
+          >
+            <div
+              className="inline-flex"
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,8 +26,19 @@ const config: Config = {
         sans: "var(--font-sans)",
         serif: "var(--font-serif)",
       },
+      animation: {
+        'ping-large': "ping-large 1s ease-in-out infinite",
+      },
+      keyframes: {
+        'ping-large': {
+          '75%, 100%': {
+            transform: 'scale(3)',
+            opacity: '0',
+          },
+        },
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-};
+}
 export default config;
