a51f507959751e64fa32ce267dc506cca3dd9451
SuperSection <soumosarkar297@gmail.com>
2025-03-22 10:41:09 +0530

    feat: enhance Card component to accept additional props to make cards sticky in the ProjectsSection

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -59,8 +59,14 @@ export const ProjectsSection = () => {
           description="See how I automate and implement solutions using DevSecOps best practices."
         />
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
-            <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20">
+          {portfolioProjects.map((project, projectIndex) => (
+            <Card
+              key={project.title}
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`,
+              }}
+            >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
                   <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
